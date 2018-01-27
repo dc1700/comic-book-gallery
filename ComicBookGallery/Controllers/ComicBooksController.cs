dd7add9f06199b8ad43001b2f30061408966e000
DECLAN CLARK <DeclanClark@Decs-MBP.home>
2018-01-27 19:51:22 +0000

    Revert "Revert "changes""
    This reverts commit 73ccbc68f1287eaf47274496bd23363614571095.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,11 +12,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail() 
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello, World!");
+            return View();
         }
     }
 }
