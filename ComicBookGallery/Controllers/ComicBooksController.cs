d32909ccc7634462f2b54f086ff1af355eb44e3a
DECLAN CLARK <DeclanClark@decs-mbp.home>
2018-01-28 14:54:57 +0000

    Updated Comic Book Detail View

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Hello from the description.</p>";
+            ViewBag.Artists = new string[]
+                          {
+                              "Script : Dan Slott",
+                              "Colors : Edgar Delgato"
+                          };
+
+
             return View();
         }
     }
